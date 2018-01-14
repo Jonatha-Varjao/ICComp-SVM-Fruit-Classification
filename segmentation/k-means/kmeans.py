249841f18704d6436a4f390415f3146e5c06963b
Jonatha-Varjao <jonathavarjao@uft.edu.br>
2018-01-14 02:12:26 -0300

    kmeans segmentation quase la

--- segmentation/k-means/kmeans.py
+++ segmentation/k-means/kmeans.py
@@ -12,50 +12,57 @@ import os
 
 class Segment:
         # construtor da classe com default de clusters = 4.
-    def __init__(self,segments=4):
+    def __init__(self, segments = 4):
         self.segments=segments
 
         # Método para segmentar a imagem usando K-means.
     def kmeans(self,image):
-        image       =   cv2.GaussianBlur(image,(5,5),0)
-        vectorized  =   image.reshape(-1,3)
-        vectorized  =   np.float32(vectorized) 
-        criteria    =   (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-        ret,label,center=cv2.kmeans(vectorized,self.segments,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
-        res = center[label.flatten()]
-        segmented_image = res.reshape((image.shape))
-        return label.reshape((image.shape[0],image.shape[1])),segmented_image.astype(np.uint8)
+        #image            =   cv2.GaussianBlur(image,(3,5),0)
+        vectorized       =   image.reshape(-1,3)
+        vectorized       =   np.float32(vectorized) 
+        criteria         =   (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        ret,label,center = cv2.kmeans(vectorized, self.segments, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+        res              = center[label.flatten()]
+        segmented_image  = res.reshape((image.shape))
+        return label.reshape((image.shape[0], image.shape[1])), segmented_image.astype(np.uint8)
 
         # Extração do K da imagem segmentada.
     def extractComponent(self,image,label_image,label):
-        component=np.zeros(image.shape,np.uint8)
-        component[label_image==label]=image[label_image==label]
+        component                     = np.zeros(image.shape, np.uint8)
+        component[label_image==label] = image[label_image==label]
         return component
         
         # Método para Segmentar imagens em um dado path
     def segmentFolder(self, folderName):
         for filename in os.listdir(folderName):
-            if '.JPG' in filename:
+            if '.' in filename:
                 print(filename)
                 image = cv2.imread(folderName+filename)  
                 label,result= seg.kmeans(image)
                 #cv2.imshow("segmenteda",result)
-                cv2.imwrite(folderName + "/Segment_Images/Segmentada_" + filename,result)
+                cv2.imwrite(folderName + "Segment_Images/kmeans/Kmeans_" + filename, result)
                 # for cluster in range(seg.segments):
                 #     result=seg.extractComponent(image, label, cluster)
                 #     cv2.imwrite("Component_"+str(cluster)+"_"+imageString[0], result)
         return seg
 
 if __name__ == "__main__":
-    
-    seg     = Segment()
-    
     # HARD-CODE dos paths das imagens
     # Trocar o path para a máquina local
-    manga   = '/home/jonatha/Documentos/PIBICWARLEY/ICComp-SVM-Fruit-Classification/database/Manga/'
-    laranja = '/home/jonatha/Documentos/PIBICWARLEY/ICComp-SVM-Fruit-Classification/database/Laranja/'
+    manga            = '/home/jonatha/Documentos/PIBICWARLEY/ICComp-SVM-Fruit-Classification/database/Manga/'
+    laranja          = '/home/jonatha/Documentos/PIBICWARLEY/ICComp-SVM-Fruit-Classification/database/LaranjaTangerina/'
     laranjaInfectada = '/home/jonatha/Documentos/PIBICWARLEY/ICComp-SVM-Fruit-Classification/database/LaranjaInfectada/'
 
-    seg.segmentFolder(manga)
+    seg              = Segment()
 
-#TODO -1: Refatorar o código e otimizar o tempo.
+    #seg.segmentFolder(manga)
+    #seg.segmentFolder(laranja)
+    seg.segmentFolder(laranjaInfectada)
+
+
+#TODO   1: Refatorar o código e otimizar o tempo.
+#TODO   2: Adicionar listar de folders no método de segmentação
+#TODO   3: Problema no filtro Gaussiano do OpenCV (widht x height) da imagem n bate no kernel escolhido
+            #  - try - catch, ou interpolar a imagem pra bater o kernel....
+#TODO   4: Ajeitar o problema do filename incluir o folder e interromper a segmentação
+            # '.jpg' in String ( apesar que a solução tá bem clean)
