60d5e39bd8147820f44e3a7a6d07a1c83b6f2d12
Jonatha-Varjao <jonathavarjao@uft.edu.br>
2018-01-21 02:33:57 -0300

    Adicionando modulo jseg na main

--- segmentation/main.py
+++ segmentation/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from extraction.extraction import * 
 from kmeans.kmeans import * 
+from jseg.jseg import *
 
 
 '''
@@ -10,21 +11,35 @@ from kmeans.kmeans import *
 if __name__ == "__main__":
     # HARD-CODE dos paths das imagens
     # Trocar o path para a máquina local
-    manga            = '/home/jonatha/Documentos/PIBICWARLEY/ICComp-SVM-Fruit-Classification/database/Manga/Extracted_Images/'
-    laranja          = '/home/jonatha/Documentos/PIBICWARLEY/ICComp-SVM-Fruit-Classification/database/LaranjaTangerina/Extracted_Images/'
-    laranjaInfectada = '/home/jonatha/Documentos/PIBICWARLEY/ICComp-SVM-Fruit-Classification/database/LaranjaInfectada/Extracted_Images'
+    manga            = '/home/jonatha/Documentos/PIBICWARLEY/ICComp-SVM-Fruit-Classification/database/Manga/'
+    macaRoyal        = '/home/jonatha/Documentos/PIBICWARLEY/ICComp-SVM-Fruit-Classification/database/MacaRoyal/'
+    macaVerde        = '/home/jonatha/Documentos/PIBICWARLEY/ICComp-SVM-Fruit-Classification/database/MacaVerde/'
+    laranja          = '/home/jonatha/Documentos/PIBICWARLEY/ICComp-SVM-Fruit-Classification/database/LaranjaTangerina/'
+    laranjaInfectada = '/home/jonatha/Documentos/PIBICWARLEY/ICComp-SVM-Fruit-Classification/database/LaranjaInfectada/'
 
-    #seg              = Segment()
-    Extraction       = Extract()
+    seg              = Segment()
+    extraction       = Extract()
+    jseg             = Jseg()
+    
+    # Teste individual com alguma imagem do database
+    #image = extraction.crop_image(extraction.extract_countor('/home/jonatha/Documentos/PIBICWARLEY/ICComp-SVM-Fruit-Classification/database/LaranjaInfectada/2013_10_25__0063.jpg'))
+    #cv2.imwrite("teste.jpg", image)
     
     # PRE-PROCESSAMENTO
     # Extração das frutas nas imagens
-    #Extraction.extract_folder(manga)
-    #Extraction.extract_folder(laranja)
-    #Extraction.extract_folder(laranjaInfectada)
+    #extraction.extract_folder(manga)
+    #extraction.extract_folder(laranja)
+    #extraction.extract_folder(laranjaInfectada)
+    #extraction.extract_folder(macaRoyal)
+    #extraction.extract_folder(macaVerde)
+
     
-    # PROCESSAMENTO DAS IMAGES (K-MEANS)
-    #seg.segmentFolder(manga)
-    #seg.segmentFolder(laranja)
-    #seg.segmentFolder(laranjaInfectada)
+    # PROCESSAMENTO DAS IMAGENS (JSEG)
+    jseg.segment_folder(macaRoyal)
+    
+    
+    # PROCESSAMENTO DAS IMAGENS (K-MEANS)
+    #seg.segment_folder(manga)
+    #seg.segment_folder(laranja)
+    #seg.segment_folder(laranjaInfectada)
 
